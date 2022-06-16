0b498ebdce475117e1cc4ae25d9dd783c78d0580
Xenia Bechthold <kseniabekhtgold@gmail.com>
2022-06-16 20:26:01 +0200

    Optimized

--- src/SecondLevel.java
+++ src/SecondLevel.java
@@ -6,21 +6,19 @@ public class SecondLevel {
         System.out.println("Enter your number");
         int number = scanner.nextInt();
 
-        if (number > 2) {
+        if (number > 1) {
             //I tried to make number-1 and number/2, but there were too many operations))
             for (int i = 2; i * i <= number; i++) {
                 if (number % i == 0) {
-                    System.out.println("It is not prime :(");
+                    System.out.println("It is composite number!");
                     return;
                 }
             }
             System.out.println("It is prime!! ;)");
 
 
-        } else if (number > 0) {
-            System.out.println("It is prime xD");
         } else {
-            System.out.println("Wrong input");
+            System.out.println("Wrong input. A prime number can only be a natural number more than 1");
         }
 
 
